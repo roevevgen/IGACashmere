95c2b0b7897cd0633eab6604bd0c29906cf2453b
roevevgen <roevevgen7@gmail.com>
2020-09-16 23:43:10 +0300

    first commit

---
+++ smartgrid.js
@@ -0,0 +1,35 @@
+module.exports = {
+    columns: 12,
+    offset: "30px",
+    // mobileFirst: true,
+    container: {
+        maxWidth: "3000px",
+        fields: "81px"
+    },
+    breakPoints: {
+        m: {
+            width: "1456px"
+        },
+        md: {
+            width: "920px",
+            fields: "15px"
+        },
+        sm: {
+            width: "740px"
+        },
+        xs: {
+            width: "576px"
+        },
+        xxs: {
+            width: "420px",
+            /*
+            offset: "10px",
+            fields: "5px"
+            */
+        },
+        xxxs: {
+            width: "320px"
+        }
+    },
+    //detailedCalc: true
+};
