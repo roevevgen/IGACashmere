0d4791e554ee6c9916aa431b74e8a2b4ccf9917f
roevevgen <roevevgen7@gmail.com>
2020-09-22 00:57:18 +0300

    end

--- src/js/main.js
+++ src/js/main.js
@@ -1,41 +1,42 @@
-// $('.menu-btn').on('click', function(e) {
-//     e.preventDefault();
-//     $('.menu').toggleClass('menu_active');
-// })
-var vm = new Vue({
+
+var sourceOfTruth = {
+    shop:'Your cart is emptyTime to start shopping!',
+    href1:'/catalog',
+    links1: 'Catalog',
+    href2:'/about',
+    links2: 'About',
+    href3:'/blog',
+    links3: 'Blog',
+    href4:'/jobs',
+    links4: 'Jobs',
+    href5:'/top',
+    links5: 'Top',
+    href6:'subscribe',
+    links6: 'Subscribe',
+    isActive: false,
+    isActivatus: false,};
+
+var vmA = new Vue({
     el: '#app',
-    data: {
-        isActive: false,
-        isActivatus:false,
-    },
+    data: sourceOfTruth,
     methods: {
-        activate()
-        {
+        activate() {
             this.isActive = !this.isActive;
-            if (this.isActive)
-            {
+            if (this.isActive) {
                 document.getElementById("mySidebar").style.width = "320px";
                 document.getElementById("main").style.marginLeft = "1px";
-            }
-
-            else
-            {
+            } else {
                 document.getElementById("mySidebar").style.width = "0px";
                 document.getElementById("main").style.marginLeft = "0px";
             }
 
         },
-        activates()
-        {
+        activates() {
             this.isActivatus = !this.isActivatus;
-            if (this.isActivatus)
-            {
+            if (this.isActivatus) {
                 document.getElementById("myShopings").style.width = "320px";
                 document.getElementById("main__shoop").style.marginLeft = "1px";
-            }
-
-            else
-            {
+            } else {
                 document.getElementById("myShopings").style.width = "0px";
                 document.getElementById("main__shoop").style.marginLeft = "0px";
             }
@@ -43,41 +44,29 @@ var vm = new Vue({
         }
     }
 
+
 });
-var vm = new Vue({
+var vmB = new Vue({
     el: '#app2',
-    data: {
-        isActive: false,
-        isActivatus:false,
-    },
+    data: sourceOfTruth,
     methods: {
-        activate()
-        {
+        activate() {
             this.isActive = !this.isActive;
-            if (this.isActive)
-            {
+            if (this.isActive) {
                 document.getElementById("mySidebar2").style.width = "320px";
                 document.getElementById("main2").style.marginLeft = "1px";
-            }
-
-            else
-            {
+            } else {
                 document.getElementById("mySidebar2").style.width = "0px";
                 document.getElementById("main2").style.marginLeft = "0px";
             }
 
         },
-        activates()
-        {
+        activates() {
             this.isActivatus = !this.isActivatus;
-            if (this.isActivatus)
-            {
+            if (this.isActivatus) {
                 document.getElementById("myShopings2").style.width = "320px";
                 document.getElementById("main__shoop2").style.marginLeft = "1px";
-            }
-
-            else
-            {
+            } else {
                 document.getElementById("myShopings2").style.width = "0px";
                 document.getElementById("main__shoop2").style.marginLeft = "0px";
             }
